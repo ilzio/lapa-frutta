1ca733819be72cac07f7d214a8db862ae538ba57
peppe <hdclsd@yahoo.it>
2020-10-22 22:27:57 +0200

    footer init

--- components/_wrapper/Layout/Footer.js
+++ components/_wrapper/Layout/Footer.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
+        <div className="Footer">
             this is footer
+            <style jsx>{`
+                .Footer{
+                    height: 56px;
+                    background-color: #0f52da;
+                    color: white;
+                    display: flex;
+                    align-items: center;
+                    box-sizing: border-box;
+                    padding: 16px
+                }
+            `}</style>
         </div>
     )
 }
