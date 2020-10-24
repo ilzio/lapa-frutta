17de3acbb0e1945240ca134cdf4491a8914470ef
peppe <hdclsd@yahoo.it>
2020-10-24 12:33:53 +0200

    home sections + fake data

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import Hero from './Hero'
+import Melo from './Melo'
+import OrderNow from './OrderNow'
+import Recipies from './Recipies'
+import Why from './Why'
+
+const cards = [
+    {
+        title: "Fresh products",
+        text: "Our tomatos are as redder than yours",
+        image: "/assets/ciliegino.jpg"
+    },
+    {
+        title: "Locally sourced",
+        text: "This is my cousin, working in the vineyard",
+        image: "/assets/zappa-vigna.jpg"
+    },
+    {
+        title: "We delivery!",
+        text: "It might get a bit slow, though...",
+        image: "/assets/lapa-front.jpeg"
+    }
+   
+]
+
+const slides = [
+    "/assets/Recipies/cannolo.jpg",
+    "/assets/Recipies/norma.jpg",
+    "/assets/Recipies/parmigiana.jpg",
+    "/assets/Recipies/puppetta.jpg",
+]
 
 const Home = () => {
     return (
         <div>
             <Hero image="/assets/image.jpeg" />
+            <Melo />
+            {/* test only */}
+            <div style={{height:'100px'}}></div> 
+            {/* test only */}
+            <Why cards={cards} />
+            {/* test only */}
+            <div style={{height:'100px'}}></div> 
+            {/* test only */}
+            <OrderNow />
+            {/* test only */}
+            <div style={{height:'100px'}}></div> 
+            {/* test only */}
+            <Recipies slides={slides} />
         </div>
     )
 }
