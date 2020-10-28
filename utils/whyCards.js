911a4a9f9946865d42ebb805b5785d1ae561f593
peppe <hdclsd@yahoo.it>
2020-10-29 00:09:27 +0100

    whyCards bgPosition key added

--- utils/whyCards.js
+++ utils/whyCards.js
@@ -3,15 +3,20 @@ export default [
     title: "Fresh products",
     text: "Our tomatos are as redder than yours",
     image: "/assets/ciliegino.jpg",
+    buttonText: "Learn more"
   },
   {
     title: "Locally sourced",
     text: "This is my cousin, working in the vineyard",
     image: "/assets/zappa-vigna.jpg",
+    buttonText: "Learn more",
+    bgPosition: 'left'
   },
   {
-    title: "We delivery!",
+    title: "We do delivery!",
     text: "It might get a bit slow, though...",
     image: "/assets/lapa-front.jpeg",
+    buttonText: "Order Now!",
+    bgPosition: 'left'
   },
 ];
