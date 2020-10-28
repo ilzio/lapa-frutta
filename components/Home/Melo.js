d30f67b87d3cd5a0e17cae2931b87507bea126a9
peppe <hdclsd@yahoo.it>
2020-10-29 00:09:10 +0100

    Why BEM + styling

--- components/Home/Melo.js
+++ components/Home/Melo.js
@@ -10,7 +10,7 @@ const Melo = () => (
       text="My name is Cammelo Scannapeco, I've been selling fruit since I was 5 years old, with my father. After learning web development at the age of 65 I started my own e-commerce and I now make 25 euros a day from my online sales. 
                 I continue a family tradition"
       button={{
-        text: "learn more",
+        text: "Learn more",
         href: "about",
       }}
     />
--- components/Home/Why.js
+++ components/Home/Why.js
@@ -3,10 +3,10 @@ import Card from "../common/Card";
 
 const Why = ({ cards }) => (
   <section className="Why">
-    <div className="sectionTitleContainer">
+    <div className="Why__title-container">
       <h2>Why choose us</h2>
     </div>
-    <div className="cardsContainer">
+    <div className="Why__cards-container">
       {cards &&
         cards.map((card) => (
           <Card
@@ -14,24 +14,36 @@ const Why = ({ cards }) => (
             title={card?.title}
             text={card?.text}
             button={card?.button}
+            bgPosition={card?.bgPosition}
           />
         ))}
     </div>
-    <style jsx>
-      {`
-            .Why{
-                height: 300px;
-            }
-            .sectionTitleContainer{
-                text-align: center;s
-            }
-            .cardsContainer{
-                height: 100%;
-                display: flex;
-                justify-content: space-around;
-            }
-        `}
-    </style>
+    <style jsx>{`
+      .Why {
+        height: auto;
+        background-color: #0f52da;
+        color: white;
+        padding: 40px 0;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+      }
+      .Why h2 {
+        text-shadow: 1.3px 1.3px black;
+      }
+      .Why__title-container {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+      .Why__cards-container {
+        width: 100%;
+        max-width: 1200px;
+        display: flex;
+        justify-content: space-between;
+      }
+    `}</style>
   </section>
 );
 
