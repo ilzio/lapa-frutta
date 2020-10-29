546957400d945a5114e7af5e61e22f961e5e8846
peppe <hdclsd@yahoo.it>
2020-10-29 22:19:56 +0100

    OrderNow style fixes

--- components/Home/OrderNow.js
+++ components/Home/OrderNow.js
@@ -6,13 +6,30 @@ const OrderNow = () => (
     <SimpleSection
       imagePosition="right"
       image="assets/lapa-casci.jpg"
+      bgPosition="left"
       title="Order now!"
       text="We deliver in eastern Sicily using only traditional transportation technologies. Get fresh fruit on your table now!"
+      textColor="#000000"
+      titleColor="#0f52da"
       button={{
         text: "Order now",
         href: "about",
       }}
     />
+    <style jsx>{`
+      .OrderNow {
+        width: 800px;
+        background-color: white;
+        background-size: 100px;
+        background-position: center;
+        background-repeat: repeat;
+        border-radius: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+      }
+    `}</style>
   </section>
 );
 
