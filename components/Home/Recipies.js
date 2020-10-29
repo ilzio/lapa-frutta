0ec1ac817a8416bc10cc5ceddab6fa534e2fd891
peppe <hdclsd@yahoo.it>
2020-10-29 22:21:03 +0100

    Recipies BEM refact.

--- components/Home/Recipies.js
+++ components/Home/Recipies.js
@@ -5,7 +5,6 @@ import Slider from "../common/Slider";
 import RecipeCard from "./RecipeCard";
 
 const Recipies = ({ recipies }) => {
- 
   const getRecipies = () =>
     recipies.map((recipie) => (
       <RecipeCard
@@ -18,18 +17,28 @@ const Recipies = ({ recipies }) => {
 
   return (
     <section className="Recipies">
-      <div className="headerCard">
+      <div className="Recipies-card-container">
         <Card
           image="/assets/cuncittina.jpeg"
           title="Cuncittina's Recipies"
-          text="Learn to cook with my wife, Cuncittina"
+          textColor="black"
+          textShadow="none"
+          titleShadow="none"
+          separator
+          separatorColor="#0f52da"
         />
       </div>
       <Slider slides={getRecipies()} />
       <style jsx>{`
-        .headerCard {
+        .Recipies-card-container {
           display: flex;
           justify-content: center;
+        }
+        .Recipies {
+          background-image: url("/assets/ficurinia.svg");
+          background-size: 70px;
+          background-repeat: repeat;
+          padding: 80px 0
         }
       `}</style>
     </section>
