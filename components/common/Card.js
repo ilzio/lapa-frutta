b32e2942849a2f7a69df85c60b24fb16f4686496
peppe <hdclsd@yahoo.it>
2020-10-29 22:17:54 +0100

    Card style fixes + added props

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,28 +1,27 @@
 import React from "react";
 import Button from "./Button";
 
-const Card = ({ title, text, image, button, bgPosition }) => {
+const Card = ({
+  title,
+  text,
+  image,
+  button,
+  bgPosition,
+  separator,
+  separatorColor,
+  titleShadow,
+  textShadow,
+  titleColor,
+  textColor,
+}) => {
   return (
     <div className="Card">
       <div className="Card__image-container" />
       <div className="Card__content-container">
-        {title && (
-          <div>
-            <h3>{title}</h3>
-          </div>
-        )}
-        {/* {separator && ( */}
-        {true && <hr className="Card__separator" />}
-        {text && (
-          <div className="Card__text-container">
-            <p>{text}</p>
-          </div>
-        )}
-        {button && (
-          <div className="Card__text-container">
-            <Button text={button.text} href={button.href} />
-          </div>
-        )}
+        {title && <h3 className="Card__title">{title}</h3>}
+        {separator && <hr className="Card__separator" />}
+        {text && <p className="Card__text">{text}</p>}
+        {button && <Button text={button.text} href={button.href} />}
       </div>
       <style jsx>{`
         .Card {
@@ -31,31 +30,36 @@ const Card = ({ title, text, image, button, bgPosition }) => {
           align-items: center;
           max-width: 250px;
         }
-        .Card h3 {
-          text-shadow: 0.5px 0.5px black;
-        }
-        .Card__content-container {
-          text-align: center;
-        }
-        .Card__text-container {
-          text-align: center;
-          font-size: 1.2em;
-        }
-        .Card__separator {
-          height: 2px;
-          background-color: #ff0;
-          border: none;
-        }
         .Card__image-container {
-          height: 160px;
-          width: 160px;
+          height: 140px;
+          width: 140px;
           background-image: url(${image});
           background-size: cover;
-          background-position: ${bgPosition || 'center'};
+          background-position: ${bgPosition || "center"};
           background-repeat: no-repeat;
           border-radius: 50%;
           margin-bottom: 16px;
           box-shadow: 0.5px 0.5px #00000063;
+        }
+        .Card__content-container {
+          text-align: center;
+        }
+        .Card__title {
+          color: ${titleColor || "#0f52da"};
+          text-shadow: ${titleShadow || `0.5px 0.5px black`};
+        }
+        .Card__separator {
+          height: 2px;
+          border: none;
+          background-color: ${separatorColor || `#ff0`};
+          width: 100%;
+        }
+        .Card__text {
+          text-align: center;
+          font-size: 1.2em;
+          color: ${textColor || "#ffffff"};
+          font-family: "Balsamiq Sans", cursive;
+          text-shadow: ${textShadow || `0.3px 0.3px black`};
         }
       `}</style>
     </div>
