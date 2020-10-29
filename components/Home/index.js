d0217d39bda14e1c31e092e6695b1bfb7c34182a
peppe <hdclsd@yahoo.it>
2020-10-29 22:19:30 +0100

    test separators removed from index page

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,30 +1,17 @@
 import React from "react";
 import Hero from "./Hero";
 import Melo from "./Melo";
-import OrderNow from "./OrderNow";
 import Recipies from "./Recipies";
 import Why from "./Why";
-import recipies from '../../utils/recipies';
-import whyCards from '../../utils/whyCards';
-
-
-
+import recipies from "../../utils/recipies";
+import whyCards from "../../utils/whyCards";
 
 const Home = () => (
   <main>
     <Hero image="/assets/image.jpeg" />
     <Melo />
-    {/* test only */}
-    <div style={{ height: "100px" }} />
-    {/* test only */}
     <Why cards={whyCards} />
-    {/* test only */}
-    <div style={{ height: "100px" }} />
-    {/* test only */}
-    <OrderNow />
-    {/* test only */}
-    <div style={{ height: "100px" }} />
-    {/* test only */}
+
     <Recipies recipies={recipies} />
   </main>
 );
