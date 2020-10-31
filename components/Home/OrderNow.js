5b931de4d5be4c7936a0becfde695b411861f766
peppe <hdclsd@yahoo.it>
2020-10-31 22:59:07 +0100

    orderNow style fix + lapa-casci img edit

--- components/Home/OrderNow.js
+++ components/Home/OrderNow.js
@@ -2,12 +2,11 @@ import React from "react";
 import { lapaBlack, lapaBlue, lapaWhite } from "../../styles/colors";
 import SimpleSection from "../common/SimpleSection";
 
-
 const OrderNow = () => (
   <section className="OrderNow">
     <SimpleSection
       imagePosition="right"
-      image="assets/lapa-casci.jpg"
+      image="assets/lapa-casci.png"
       bgPosition="left"
       title="Order now!"
       text="We deliver in eastern Sicily using only traditional transportation technologies.  Get fresh fruit on your table now!"
@@ -20,7 +19,7 @@ const OrderNow = () => (
     />
     <style jsx>{`
       .OrderNow {
-        width: 800px;
+        max-width: 800px;
         background-color: ${lapaWhite};
         background-size: 100px;
         background-position: center;
@@ -29,6 +28,7 @@ const OrderNow = () => (
         display: flex;
         justify-content: center;
         align-items: center;
+        padding: 0px 16px;
         box-sizing: border-box;
       }
     `}</style>
