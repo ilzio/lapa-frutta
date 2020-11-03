1ab02a968b8c536d3b3ca7bfb305ab5118ce182c
peppe <hdclsd@yahoo.it>
2020-11-03 18:09:59 +0100

    why component refactored + responsive init

--- components/Home/Why.js
+++ components/Home/Why.js
@@ -1,59 +1,72 @@
 import React from "react";
-import { lapaBlue, lapaWhite } from "../../styles/colors";
 import Card from "../common/Card";
+import Slider from "../common/Slider";
 import OrderNow from "./OrderNow";
+import { lapaBlue, lapaWhite } from "../../styles/colors";
+import themeBreakpoints from "../../styles/breakpoints";
 
-const Why = ({ cards }) => (
-  <section className="Why">
-    <h2 className="Why__title">Why choose us</h2>
-    <div className="Why__cards-container">
-      {cards &&
-        cards.map((card) => (
-          <Card
-            image={card?.image}
-            title={card?.title}
-            text={card?.text}
-            button={card?.button}
-            bgPosition={card?.bgPosition}
-            textColor={lapaWhite}
-            titleColor={lapaWhite}
-            separator
-            separatorColor={lapaWhite}
-          />
-        ))}
-    </div>
-    <OrderNow />
-    <style jsx>{`
-      .Why {
-        height: auto;
-        padding: 40px 0;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-evenly;
-        background-color: ${lapaBlue};
-        background-image: url("/assets/sicilia.svg");
-        background-size: 100px;
-        background-position: center;
-        background-repeat: repeat;
-      }
+function getCards(cardsFromProps) {
+  return cardsFromProps.map((card) => (
+    <Card
+      image={card?.image}
+      title={card?.title}
+      text={card?.text}
+      button={card?.button}
+      bgPosition={card?.bgPosition}
+      textColor={lapaWhite}
+      titleColor={lapaWhite}
+      separator
+      separatorColor={lapaWhite}
+    />
+  ));
+}
 
-      .Why__title {
-        text-align: center;
-        margin-bottom: 32px;
-        color: ${lapaWhite};
-        text-shadow: 1.3px 1.3px black;
-      }
-      .Why__cards-container {
-        width: 100%;
-        max-width: 1200px;
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 40px;
-      }
-    `}</style>
-  </section>
-);
+const Why = ({ cards, breakpoint }) => {
+  const cardsToDisplay = getCards(cards);
+
+  return (
+    <section className="Why">
+      <h2 className="Why__title">Why choose us</h2>
+      {breakpoint === themeBreakpoints.tablet ? (
+        <Slider autoplay slides={cardsToDisplay} />
+      ) : (
+        <div className="Why__cards-container">{cardsToDisplay}</div>
+      )}
+
+      <OrderNow />
+      <style jsx>{`
+        .Why {
+          height: auto;
+          padding: 40px 0;
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-evenly;
+          background-color: ${lapaBlue};
+          background-image: url("/assets/sicilia.svg");
+          background-size: 100px;
+          background-position: center;
+          background-repeat: repeat;
+          overflow: hidden;
+        }
+
+        .Why__title {
+          text-align: center;
+          margin-bottom: 32px;
+          color: ${lapaWhite};
+          text-shadow: 1.3px 1.3px black;
+        }
+        .Why__cards-container {
+          width: 100%;
+          max-width: 1200px;
+          display: flex;
+          justify-content: space-between;
+          margin-bottom: 40px;
+        }
+      `}</style>
+    </section>
+  );
+};
 
 export default Why;
