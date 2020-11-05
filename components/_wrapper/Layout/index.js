82ecf873745b42c0d0e2d6263d8bcd3328a2dd50
peppe <hdclsd@yahoo.it>
2020-11-05 21:42:31 +0100

    layout fixes

--- components/_wrapper/Layout/index.js
+++ components/_wrapper/Layout/index.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout = ({children}) => {
-    return (
-      <div className="Layout">
-        <Header />
-        <div className="contentContainer">{children}</div>
-        <Footer />
-        <style jsx>{`
-          .Layout {
-            height: 100%;
-            width: 100%;
-            border: solid, black, 2px;
-          }
-        `}</style>
-      </div>
-    );
-}
+const Layout = ({ children, breakpoint }) => {
+  return (
+    <div className="Layout">
+      <Header breakpoint={breakpoint} />
+      <div className="contentContainer">{children}</div>
+      <Footer />
+      <style jsx>{`
+        .Layout {
+          height: 100%;
+          width: 100%;
+          border: solid, black, 2px;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Layout;
