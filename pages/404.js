22224b232c6d4f70f77eb057cebbe2e292a48c3c
peppe <hdclsd@yahoo.it>
2020-11-09 17:17:17 +0100

    404 ready

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,74 @@
+import React from "react";
+import { lapaBlack, lapaBlue, lapaWhite, lapaYellow } from "../styles/colors";
 
-const Custom404= () => {
-  return(
+const Custom404 = () => {
+  return (
     <div className="Custom404">
-        <h1> 404 Page not found!</h1>
-    <style jsx>{`
+      <div className="Custom404__overlay" />
+      <div className="Custom404__image-container">
+        <img
+          className="Custom404__image"
+          src="/assets/cuncittina.jpeg"
+          alt="Cuncittina"
+        />
+      </div>
+      <div className="Custom404__centered-container">
+        <h1 className="Custom404__title">This page does not exist!</h1>
+      </div>
+      <style jsx>{`
         .Custom404 {
-            height: 100vh;
-            width: 100%;
-            background-image: url('/assets/muro.jpg');
-            background-size: cover;
-            background-repeat: no-repeat;
-            display: flex;
-            align-items: center;
-            justify-content: center;
+          height: calc(100vh - 112px);
+          background-color: ${lapaWhite};
+          position: relative;
         }
-        .Custom404 > h1 {
-            color: white;
+        .Custom404__centered-container {
+          width: 100%;
+          height: 100%;
+          position: absolute;
+          top: 0;
+          left: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: ${lapaBlue}
         }
-    `}</style>    
-    </div>
-  ) 
-}
+        .Custom404__title {
+          color: ${lapaYellow};
+          text-align: center;
+          text-shadow: 2px 1px ${lapaBlack};
+          margin-top: 24px;
+          z-index: 3;
+          
+        }
+        .Custom404__image {
+          height: 120px;
+          width: 120px;
+          border-radius: 50%;
+          animation: y 3s linear infinite alternate;
+          position: relative;
+          z-index: 2;
 
-export default Custom404
+        }
+        .Custom404__image-container {
+          animation: x 8s linear infinite alternate;
+          z-index: 2;
+          position: relative;
+        }
+
+        @keyframes y {
+          100% {
+            transform: translateY(calc((100vh - 112px) - 120px));
+          }
+        }
+
+        @keyframes x {
+          100% {
+            transform: translateX(calc(100vw - 120px));
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Custom404;
